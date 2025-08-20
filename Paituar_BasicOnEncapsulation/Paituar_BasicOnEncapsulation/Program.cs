11b6b02d98dd8d8d8645ff033267b0d1b951a612
ACT-STUDENT <ACT-STUDENT@LAB3-PC46>
2025-08-20 10:51:37 +0800

    Exercise 4 Basic Encapsulation

---
+++ Paituar_BasicOnEncapsulation/Paituar_BasicOnEncapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paituar_BasicOnEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Judy Ann Paituar
+            //Section: IT 301
+            //Exercise title: Basics on Encapsulation
+
+            //Intantiate an Object 
+            Shoes shoe1 = new Shoes();
+
+            //Use encapsulated properties to assign value for fields
+            shoe1.Brand = "Nike";
+            shoe1.Model = "Air Max series ";
+
+            //Call Method to display shoe Details
+            shoe1.DisplayShoeInfo();
+        }
+    }
+}
---
+++ Paituar_BasicOnEncapsulation/Paituar_BasicOnEncapsulation/Shoes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paituar_BasicOnEncapsulation
+{
+    internal class Shoes
+    {
+        //Declare private fields
+        private string brand;
+        private string model;
+
+        //Public properties
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        //Method to display shoe Information
+        public void DisplayShoeInfo()
+        {
+            Console.WriteLine("\tShoe Details");
+            Console.WriteLine("Shoe Brand: " + brand);
+            Console.WriteLine("Shoe Model: " + model);
+            Console.ReadKey();
+        }
+    }
+}
