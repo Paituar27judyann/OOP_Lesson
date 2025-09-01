935e1258adaf2300590205c98705a1c84ac94646
Student <Student@DESKTOP-CE64I1Q>
2025-09-01 20:49:21 +0800

    BasicInheritance

---
+++ inheritance_paituar/inheritance_paituar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_paituar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Judy Ann Paituar
+            //Section: IT 301
+            //Exercise title: Basic Inheritance
+
+            // Object for Regular Student
+            RegularStudent section = new RegularStudent();
+            section.Name = "Student 1";
+            section.Program = "BSIT";
+            section.Section = "IT301";
+
+            section.BasicInfo();          // Inherit method
+            section.SectionEnrolled();    // Regular students specific method
+
+
+            // Object for Irregular Student
+            IrregularStudent unitsEnrolled = new IrregularStudent();
+            unitsEnrolled.Name = "Student 2";
+            unitsEnrolled.Program = "BSIT";
+            unitsEnrolled.UnitsEnrolled = 18;
+
+            unitsEnrolled.BasicInfo();         // Inherit method
+            unitsEnrolled.EnrolledSemUnits();  // irregular students specific method
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
---
+++ inheritance_paituar/inheritance_paituar/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_paituar
+{
+    internal class Student
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        // Method to display student infoS
+        public void BasicInfo()
+        {
+            Console.WriteLine("\n\tStudent Information:");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+        }
+    }   
+    class RegularStudent : Student         // Derived Class
+    {
+        public string Section { get; set; }
+
+        // Method specific to Regular Student
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section: " + Section);
+        }
+    }
+    class IrregularStudent : Student      // Derived Class
+    {
+        public int UnitsEnrolled { get; set; }
+
+        // Method specific to IrregularStudent
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("Units Enrolled: " + UnitsEnrolled);
+        }
+    }
+
+
+}
