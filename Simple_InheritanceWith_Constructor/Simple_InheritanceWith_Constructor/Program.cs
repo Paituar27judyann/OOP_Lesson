1e836ddd99f0a980bcb8fa2e21fcfd699f8c20e8
ACT-STUDENT <ACT-STUDENT@LAB3-PC46>
2025-09-15 10:20:13 +0800

    folder

---
+++ Simple_InheritanceWith_Constructor/Simple_InheritanceWith_Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_InheritanceWith_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Judy Ann Paituar
+            //Section: IT 301
+            //Exercise title: Simple Inheritance with Constructor
+
+            // Object for Regular Student
+            RegularStudent section = new RegularStudent("Student 1", "BSIT", "IT301");
+
+            section.BasicInfo();          // Inherited method
+            section.SectionEnrolled();    // Regular student specific method
+
+
+            // Object for Irregular Student
+            IrregularStudent unitsEnrolled = new IrregularStudent("Student 2", "BSIT", 18);
+
+            unitsEnrolled.BasicInfo();         // Inherited method
+            unitsEnrolled.EnrolledSemUnits();  // Irregular student specific method
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Simple_InheritanceWith_Constructor/Simple_InheritanceWith_Constructor/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_InheritanceWith_Constructor
+{
+    internal class Student  // Base class
+    {
+
+        // Variables (fields)
+        public string name;
+        public string program;
+
+        // Constructor
+        public Student(string nName, string nProgram)
+        {
+            this.name = nName;
+            this.program = nProgram;
+        }
+
+        // Method to display student info
+        public void BasicInfo()
+        {
+            Console.WriteLine("\n\tStudent Information:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Program: " + program);
+        }
+    }
+
+    class RegularStudent : Student   // Derived Class
+    {
+        // Variable for regular students
+        public string section;
+
+        // Constructor of derived class with base class call
+        public RegularStudent(string name, string program, string section)
+            : base(name, program)
+        {
+            this.section = section;
+        }
+
+        // Method specific to Regular Student
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section: " + section);
+        }
+    }
+
+    class IrregularStudent : Student   // Derived Class
+    {
+        // Variable for irregular students
+        public int unitsEnrolled;
+
+        // Constructor of derived class with base class call
+        public IrregularStudent(string name, string program, int unitsEnrolled)
+            : base(name, program)
+        {
+            this.unitsEnrolled = unitsEnrolled;
+        }
+
+        // Method specific to Irregular Student
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("Units Enrolled: " + unitsEnrolled);
+        }
+    }
+}
