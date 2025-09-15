b69859628be3c76e80c4d0c0544822a26cfb65e4
ACT-STUDENT <ACT-STUDENT@LAB3-PC46>
2025-09-15 10:30:27 +0800

    folder

---
+++ Constructor_With_User_Input/Constructor_With_User_Input/KoreanBoyGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_With_User_Input
+{
+    internal class KoreanBoyGroup
+    {
+        // Fields
+        public string name, agency;
+        public int debutyear;
+
+        // Method to display group info
+        public void DisplayGroupInfo()
+        {
+            Console.WriteLine("\nGroup name: " + name + "\nAgency: " + agency + "\nYear Debuted: " + debutyear);
+        }
+
+        public KoreanBoyGroup(string name, string agency, int debutyear)
+        {
+            // Parameterized Constructor
+            this.name = name;
+            this.agency = agency;
+            this.debutyear = debutyear;
+        }
+    }
+}
---
+++ Constructor_With_User_Input/Constructor_With_User_Input/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_With_User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Name: Judy Ann Paituar
+            //Section: IT301
+            //Exercise: User Input with Constructor
+
+
+            // Ask user for the first group's details
+            Console.WriteLine("Enter details for the first K-pop group:");
+            Console.Write("Name: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Agency: ");
+            string agency1 = Console.ReadLine();
+            Console.Write("Debut Year: ");
+            int year1 = int.Parse(Console.ReadLine());
+
+
+            // Ask user for the second group's details
+            Console.WriteLine("\nEnter details for the second K-pop group:");
+            Console.Write("Name: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Agency: ");
+            string agency2 = Console.ReadLine();
+            Console.Write("Debut Year: ");
+            int year2 = int.Parse(Console.ReadLine());
+
+
+            // Display information of both groups
+            Console.WriteLine("\n--- Group Details ---");
+
+            // instantiate an object for the first group using parameterized constructor
+            KoreanBoyGroup group1 = new KoreanBoyGroup("Exo", "SM Entertainment", 2012);
+            group1.DisplayGroupInfo();
+
+            //instantiate an object for the first group   using parameterized constructor
+            KoreanBoyGroup group2 = new KoreanBoyGroup("BTS", "Big Hit Entertainment", 2013);
+            group2.DisplayGroupInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
